f26e6f87fcd8da618701e8eb6d396777e7c74cc8
HosseinHeris <hokhadiv@microsoft.com>
2024-02-07 12:14:07 -0800

    update the code to do only vector search, also add vectorsearch type such as L2

--- package/pfazuredb/tools/postgrespg.py
+++ package/pfazuredb/tools/postgrespg.py
@@ -1,7 +1,6 @@
 from promptflow import tool
 from promptflow.connections import CustomConnection
 
-
 @tool
 def vectorsearch(
     connection: CustomConnection,
@@ -9,22 +8,35 @@ def vectorsearch(
     search_type: str,
     num_results: int,
     embeddings: list,
+    vectorsearch_method: str,
     filter_text: str,
-) -> str:
+) -> list:
     from pgvector.psycopg2 import register_vector
     from psycopg2 import Error
-    from psycopg2 import sql
     import numpy as np
     import psycopg2
+    import json
 
     pgconnection = psycopg2.connect(connection.configs["conn_string"])
     register_vector(pgconnection)
+
+    if vectorsearch_method == "L2":
+        distance_operator = "<->"
+    elif vectorsearch_method == "Cosine":
+        distance_operator = "<=>"
+    elif vectorsearch_method == "Inner":
+        distance_operator = "<#>"
+    else:
+        raise Error(
+            f"Vector search method '{vectorsearch_method}' is not implemented. Please choose one of: L2, Cosine, Inner."
+        )
+    
     if search_type == "vector":
         select_query = (
-            f"SELECT id FROM {table_name} ORDER BY embedding <-> %s LIMIT {num_results}"
+            f"SELECT * FROM {table_name} ORDER BY embedding {distance_operator} %s LIMIT {num_results}"
         )
     elif search_type == "hybrid":
-        select_query = f"SELECT id FROM {table_name} where {filter_text} ORDER BY embedding <-> %s LIMIT {num_results}"
+        select_query = f"SELECT * FROM {table_name} where {filter_text} ORDER BY embedding {distance_operator} %s LIMIT {num_results}"
     else:
         raise Error(
             f"search_type{search_type} is not implemented. Please choose vector or hybrid"
@@ -33,23 +45,19 @@ def vectorsearch(
     cursor = pgconnection.cursor()
     cursor.execute(select_query, (np.array(embeddings),))
     results = cursor.fetchall()
-    top_ids = []
-    for i in range(len(results)):
-        top_ids.append(int(results[i][0]))
 
-    pgconnection.rollback()
-    format_ids = ", ".join(["%s"] * len(top_ids))
-    sql = f"SELECT CONCAT('content: ', content, ' meta_data:[', 'symbol: ', symbol, ', fiscal year: ', fiscalyear, ', fiscal quarter: ', fiscalquarter, ', source: ', source, ' ]') AS concat FROM {table_name} WHERE id IN ({format_ids})"
-    
-    try:
-        cursor.execute(sql, top_ids)
-        top_rows = cursor.fetchall()
-    except (Exception, Error) as e:
-        print(f"Error executing SELECT statement: {e}")
-    finally:
-        if cursor:
-            cursor.close()
-        if connection:
-            pgconnection.close()
-    retrieved_results = [row[0] for row in top_rows]
+    retrieved_results = []
+    for row in results:
+        row_data = {}
+        for idx, col in enumerate(cursor.description):
+            # Check if the value is serializable, otherwise convert it to a string
+            if isinstance(row[idx], np.ndarray):
+                row_data[col.name] = row[idx].tolist()
+            else:
+                row_data[col.name] = row[idx]
+        retrieved_results.append(row_data)
+
+    cursor.close()
+    pgconnection.close()
+
     return retrieved_results
